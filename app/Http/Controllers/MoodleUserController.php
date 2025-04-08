2e23cb4d9a047baa35db44da51480d5ecde66fbe
Sanilal <sanilal@gmail.com>
2025-04-08 16:48:39 +0400

    pagination added to user list

--- app/Http/Controllers/MoodleUserController.php
+++ app/Http/Controllers/MoodleUserController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Services\MoodleApiService;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 class MoodleUserController extends Controller
 {
@@ -17,28 +19,51 @@ class MoodleUserController extends Controller
     }
 
     // Fetch all users from Moodle excluding already added ones
-    public function fetchUsers()
+    public function fetchUsers(Request $request)
     {
         $users = $this->moodleApi->getUsers(); // Fetch all users
 
    
 
         if (empty($users['users'])) {
-            return view('moodle.users', ['users' => []]);
+            return view('moodle.users', ['users' => [], 'pagination' => null]);
         }
 
         // Get emails of users already added to Laravel
-        $existingUsers = User::pluck('email')->toArray();
+      //  $existingUsers = User::pluck('email')->toArray();
+
+      $existingEmails = \DB::table('trainer_profiles')->pluck('email')->toArray();
+        $existingUserIds = \DB::table('trainer_profiles')->pluck('user_id')->toArray();
+
 
         // Filter out users that are already in Laravel
-        $filteredUsers = array_filter($users['users'], function ($user) use ($existingUsers) {
-            return !in_array($user['email'], $existingUsers) && 
-                   $user['username'] !== 'guest' && 
-                   !$user['suspended'] && 
+        $filteredUsers = array_filter($users['users'], function ($user) use ($existingEmails, $existingUserIds) {
+            return !in_array($user['email'], $existingEmails) &&
+                   !in_array($user['id'], $existingUserIds) &&
+                   $user['username'] !== 'guest' &&
+                   !$user['suspended'] &&
                    $user['confirmed'];
         });
 
-        return view('moodle.users', ['users' => $filteredUsers]);
+        // Convert to Laravel Collection for easier handling
+        $userCollection = collect($filteredUsers);
+
+        // Pagination setup
+    $currentPage = LengthAwarePaginator::resolveCurrentPage();
+    $perPage = 10; // Change as needed
+    $currentPageUsers = $userCollection->slice(($currentPage - 1) * $perPage, $perPage)->all();
+
+    $paginatedUsers = new LengthAwarePaginator(
+        $currentPageUsers,
+        $userCollection->count(),
+        $perPage,
+        $currentPage,
+        ['path' => $request->url(), 'query' => $request->query()]
+    );
+
+    $paginatedUsers->withPath($request->url())->onEachSide(1);
+
+        return view('moodle.users', ['users' => $paginatedUsers]);
     }
 
     // Store a Moodle user in Laravel database
--- resources/views/moodle/users.blade.php
+++ resources/views/moodle/users.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 <div class="container">
     <h2>Moodle Users</h2>
+    <p>Showing {{ $users->firstItem() }} to {{ $users->lastItem() }} of {{ $users->total() }} users</p>
+
     <table class="table">
         <thead>
             <tr>
@@ -15,20 +17,25 @@
         </thead>
         <tbody>
             @foreach($users as $user)
-                <tr>
-                    <td><img src="{{ $user['profileimageurl'] }}" class="rounded-full small-thumb" alt="{{ $user['username'] }}"></td>
-                    <td>{{ $user['username'] }}</td>
-                    <td>{{ $user['fullname'] }}</td>
-                    <td>{{ $user['email'] }}</td>
-                    <td>
-                        <a href="../trainers/create/{{ $user['id'] }}" class="btn btn-success add-user-btn" data-username="{{ $user['username'] }}" data-fullname="{{ $user['fullname'] }}" data-email="{{ $user['email'] }}">
-                            Add User
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
+    <tr>
+        <td><img src="{{ $user['profileimageurl'] }}" class="rounded-full small-thumb" alt="{{ $user['username'] }}"></td>
+        <td>{{ $user['username'] }}</td>
+        <td>{{ $user['fullname'] }}</td>
+        <td>{{ $user['email'] }}</td>
+        <td>
+            <a href="../trainers/create/{{ $user['id'] }}" class="btn btn-success add-user-btn" data-username="{{ $user['username'] }}" data-fullname="{{ $user['fullname'] }}" data-email="{{ $user['email'] }}">
+                Add User
+            </a>
+        </td>
+    </tr>
+@endforeach
+
         </tbody>
     </table>
+   
+    <div class="d-flex justify-content-center mt-4">
+        {{ $users->links('pagination::bootstrap-4') }}
+    </div>
 </div>
 
 <script>
