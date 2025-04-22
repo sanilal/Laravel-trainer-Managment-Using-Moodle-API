c1194feb0637a9454fb203088d7ec82914ebd831
Sanilal <sanilal@gmail.com>
2025-04-22 19:49:49 +0400

    ui partialy done

--- resources/views/trainers/specializations/create.blade.php
+++ resources/views/trainers/specializations/create.blade.php
@@ -96,182 +96,7 @@
                 <select name="specialization" class="form-control">
                     <option value="">Select Specialization</option>
             
-                    <optgroup label="🏗️ Engineering Specializations">
-                        <option>Architecture</option>
-                        <option>Civil Engineering</option>
-                        <option>Electrical Engineering</option>
-                        <option>Mechanical Engineering</option>
-                        <option>Industrial Engineering</option>
-                        <option>Computer Engineering</option>
-                        <option>Computer Science</option>
-                        <option>Information Technology</option>
-                        <option>Cybersecurity</option>
-                        <option>Artificial Intelligence</option>
-                        <option>Chemical Engineering</option>
-                        <option>Software Engineering</option>
-                    </optgroup>
-            
-                    <optgroup label="📚 Literary and Humanities Specializations">
-                        <option>Arabic Language</option>
-                        <option>English Language</option>
-                        <option>History</option>
-                        <option>Geography</option>
-                        <option>Media</option>
-                        <option>Public Relations</option>
-                        <option>Journalism and Publishing</option>
-                        <option>Psychology</option>
-                        <option>Social Work</option>
-                        <option>Sociology</option>
-                        <option>Education</option>
-                    </optgroup>
-            
-                    <optgroup label="💼 Administrative and Financial Specializations">
-                        <option>Business Administration</option>
-                        <option>Accounting</option>
-                        <option>Marketing</option>
-                        <option>Economics</option>
-                        <option>Finance</option>
-                        <option>Management Information Systems</option>
-                        <option>Human Resources</option>
-                        <option>Public Administration</option>
-                    </optgroup>
-            
-                    <optgroup label="🎨 Art and Design Specializations">
-                        <option>Graphic Design</option>
-                        <option>Interior Design</option>
-                        <option>Fine Arts</option>
-                        <option>Photography</option>
-                        <option>Fashion Design</option>
-                        <option>Film Production</option>
-                    </optgroup>
-            
-                    <optgroup label="📈 Business and Marketing Specializations">
-                        <option>International Trade</option>
-                        <option>Digital Marketing</option>
-                        <option>Project Management</option>
-                        <option>Supply Chain (Logistics)</option>
-                        <option>E-Commerce</option>
-                        <option>Business Data Analysis</option>
-                        <option>Business Development</option>
-                    </optgroup>
-            
-                    <optgroup label="🌍 Environmental and Agricultural Specializations">
-                        <option>Agriculture</option>
-                        <option>Environmental Science</option>
-                        <option>Natural Resource Management</option>
-                        <option>Ocean Sciences</option>
-                        <option>Renewable Energy</option>
-                        <option>Agricultural Engineering</option>
-                        <option>Aerospace</option>
-                    </optgroup>
-            
-                    <optgroup label="⚖️ Legal and Political Specializations">
-                        <option>Civil Law</option>
-                        <option>Criminal Law</option>
-                        <option>International Law</option>
-                        <option>Human Rights</option>
-                        <option>Public Policy</option>
-                        <option>Political Science</option>
-                        <option>International Relations</option>
-                        <option>Crisis and Disaster Management</option>
-                    </optgroup>
-            
-                    <optgroup label="🧠 Brain and Behavioral Sciences Specializations">
-                        <option>Neuroscience</option>
-                        <option>Neuropsychology</option>
-                        <option>Behavioral Science</option>
-                        <option>Autism Studies</option>
-                        <option>Addiction Studies</option>
-                        <option>Psychotherapy</option>
-                    </optgroup>
-            
-                    <optgroup label="🏛️ Social and Intellectual Specializations">
-                        <option>Philosophy</option>
-                        <option>Comparative Literature</option>
-                        <option>Cultural Studies</option>
-                        <option>Anthropology</option>
-                        <option>Middle Eastern Studies</option>
-                        <option>Religious Studies</option>
-                        <option>Performing Arts</option>
-                    </optgroup>
-            
-                    <optgroup label="⚙️ Mechatronics and Robotics Specializations">
-                        <option>Mechatronics Engineering</option>
-                        <option>Robotics</option>
-                        <option>Automation and Control</option>
-                        <option>3D Printing</option>
-                        <option>Embedded Systems</option>
-                    </optgroup>
-            
-                    <optgroup label="💻 Programming and Modern Technology Specializations">
-                        <option>Game Development</option>
-                        <option>Data Analysis</option>
-                        <option>Programming (Java, Python, C++)</option>
-                        <option>App Development</option>
-                        <option>Web Development</option>
-                        <option>Networking</option>
-                        <option>Embedded Software Development</option>
-                    </optgroup>
-            
-                    <optgroup label="🌐 Languages and Translation Specializations">
-                        <option>Linguistics</option>
-                        <option>Simultaneous Interpretation</option>
-                        <option>Literary Translation</option>
-                        <option>Technical Translation</option>
-                        <option>Legal Translation</option>
-                        <option>Eastern Languages (e.g., Chinese, Japanese, Korean)</option>
-                    </optgroup>
-            
-                    <optgroup label="💡 Modern Technical and Engineering Specializations">
-                        <option>Software Engineering</option>
-                        <option>Artificial Intelligence</option>
-                        <option>Data Science and Analytics</option>
-                        <option>Mobile App Development</option>
-                        <option>Virtual and Augmented Reality (VR/AR)</option>
-                        <option>Smart Systems</option>
-                        <option>Tech Systems Management</option>
-                        <option>Embedded Systems</option>
-                    </optgroup>
-            
-                    <optgroup label="🎭 Arts and Media Specializations">
-                        <option>Film and TV Production</option>
-                        <option>Digital Media</option>
-                        <option>Screenwriting</option>
-                        <option>Sound and Music Design</option>
-                        <option>Advertising and Public Relations</option>
-                        <option>Radio and TV Production</option>
-                        <option>Digital Game Design</option>
-                        <option>Animation</option>
-                        <option>Expressive Arts and Dance</option>
-                    </optgroup>
-            
-                    <optgroup label="🚀 Space and Aviation Specializations">
-                        <option>Space Engineering</option>
-                        <option>Aeronautical Engineering</option>
-                        <option>Space Science</option>
-                        <option>Astrophysics</option>
-                        <option>Space Research and Exploration</option>
-                    </optgroup>
-            
-                    <optgroup label="💼 Management and Economics Specializations">
-                        <option>International Project Management</option>
-                        <option>International Economics</option>
-                        <option>Risk Management</option>
-                        <option>Political Economy</option>
-                        <option>Strategic Marketing</option>
-                        <option>Financial Management</option>
-                        <option>Financial and Managerial Accounting</option>
-                    </optgroup>
-            
-                    <optgroup label="🧩 Mind and Behavior Studies Specializations">
-                        <option>Behavioral Neuroscience</option>
-                        <option>Psychiatry</option>
-                        <option>Social Psychology</option>
-                        <option>Psychoanalysis</option>
-                        <option>Addiction and Treatment</option>
-                        <option>Autism Studies</option>
-                        <option>Art and Music Therapy</option>
-                    </optgroup>
+                    @include('components.specialization-options')
                     
                 </select>
                     </div>
@@ -381,29 +206,57 @@
                </div>
     </div>
 </div>
-
-        <div class="mb-3">
+<div class="row">
+    <div class="col-md-6">
+        <div class="form-group">
             <label class="form-label">Name of the Institution</label>
             <input type="text" name="cert_name_of_the_institution" class="form-control">
         </div>
-
-        <div class="mb-3">
+    </div>
+    <div class="col-md-3">
+        <div class="form-group">
             <label class="form-label">Start Date</label>
-            <input type="date" name="cert_start_date" class="form-control">
+            
+            <div class="input-group date-picker-group">
+                <input type="date" name="cert_start_date" class="form-control date-input">
+                <button class="btn btn-outline-secondary calendar-button" type="button">
+                    <i class="fa fa-calendar-alt"></i>
+                </button>
+            </div>
         </div>
-
-        <div class="mb-3">
+    </div>
+    <div class="col-md-3">
+        <div class="form-group">
             <label class="form-label">End Date</label>
-            <input type="date" name="cert_end_date" class="form-control">
+            <div class="input-group date-picker-group">
+                <input type="date" name="cert_end_date" class="form-control date-input">
+                <button class="btn btn-outline-secondary calendar-button" type="button">
+                    <i class="fa fa-calendar-alt"></i>
+                </button>
+            </div>
         </div>
-
-        <div class="mb-3">
+    </div>
+</div>
+    <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-3">
+        <div class="form-group">
             <label class="form-label">Upload Certificate</label>
             <input type="file" name="cert_upload_certificate" class="form-control">
         </div>
-
+    </div>
+    <div class="col-md-3">
         <button type="button" class="btn btn-primary" id="add-certification">Save & Add More</button>
     </form>
+    </div>
+    <div class="col-md-3"></div>    
+    </div>    
+
+ 
+
+        
+
+       
   <!-- Added Certifications -->
     <div class="mt-4">
         <h5>Added Certifications</h5>
